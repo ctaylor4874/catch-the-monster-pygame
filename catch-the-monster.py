e325e9ff461432a83d014a04a1e7495620eb9cbc
ctaylor4874 <ctaylor4874@gmail.com>
2016-12-13 10:20:59 -0600

    Added Buffer Check for Monster
    Added Buffer Check for Monster

--- catch-the-monster.py
+++ catch-the-monster.py
@@ -6,7 +6,7 @@ KEY_UP = 273
 KEY_DOWN = 274
 KEY_LEFT = 276
 KEY_RIGHT = 275
-
+#class to create a character
 class Characters(object):
     def __init__(self, charImage, positionX, positionY, screen, width, height):
         self.width = width
@@ -20,8 +20,17 @@ class Characters(object):
         self.goblin_x = randint(50, 440)
         self.goblin_y = randint(50, 440)
         self.goblin_direction = randint(0,3)
-
-    def buffCheck(self, goblin_x, goblin_y):
+    #buffer to make sure monster does not spawn on hero
+    def buffMonst(self, monster_x, monster_y):
+        hero_x = 255
+        hero_y = 240
+        buffMonst = math.sqrt((hero_x - self.monster_x)**2 + (hero_y - self.monster_y)**2)
+        while buffMonst < 200:
+            self.monster_x = randint(50, 440)
+            self.monster_y = randint(50, 440)
+            buffMonst = math.sqrt((hero_x - self.monster_x)**2 + (hero_y - self.monster_y)**2)
+    #buffer to make sure goblins do not spawn on hero
+    def buffGob(self, goblin_x, goblin_y):
         hero_x = 255
         hero_y = 240
         buffGob = math.sqrt((hero_x - self.goblin_x)**2 + (hero_y - self.goblin_y)**2)
@@ -29,7 +38,7 @@ class Characters(object):
             self.goblin_x = randint(50, 440)
             self.goblin_y = randint(50, 440)
             buffGob = math.sqrt((hero_x - self.goblin_x)**2 + (hero_y - self.goblin_y)**2)
-
+    #update goblin direction
     def updateGoblin(self, change_dir_countdown):
         if change_dir_countdown == 0:
             self.goblin_direction = randint(0,3)
@@ -43,7 +52,7 @@ class Characters(object):
             self.goblin_x-=.5
 
         self.screen.blit(self.charImage, (self.goblin_x, self.goblin_y))
-
+        #enable goblins to pass through boundary to the other side of the game
         if self.goblin_x > self.width - 30:
             self.goblin_x = 30
         if self.goblin_x < 0:
@@ -52,7 +61,7 @@ class Characters(object):
             self.goblin_y = 30
         if self.goblin_y < 0:
             self.goblin_y = self.height - 30
-
+    #update monster direction
     def updateMonster(self, change_dir_countdown, direction):
         self.change_dir_countdown = change_dir_countdown
         self.direction = direction
@@ -70,7 +79,7 @@ class Characters(object):
             self.monster_x-=1
 
         self.screen.blit(self.charImage, (self.monster_x, self.monster_y))
-
+        #enable monster to pass through boundary to the other side of the game
         if self.monster_x > self.width - 30:
             self.monster_x = 30
         if self.monster_x < 0:
@@ -79,10 +88,11 @@ class Characters(object):
             self.monster_y = 30
         if self.monster_y < 0:
             self.monster_y = self.height - 30
-
+        #countdown to change monster direction
         self.change_dir_countdown-=1
+        #return goblin direction and countdown number
         return self.change_dir_countdown, self.direction
-
+    #make sure hero cannot pass through boudary and stops at boundary
     def updateHero(self):
         self.screen.blit(self.charImage, (self.hero_x, self.hero_y))
 
@@ -121,7 +131,6 @@ def main():
     # PUT INITIALIZATION CODE HERE #
     ################################
     change_dir_countdown = 0
-    change_dir_countdown_goblin = 0
     direction = 0
     goblin_direction = 0
     lost = False
@@ -131,17 +140,19 @@ def main():
     myGoblins = [
         Characters(goblin_image, 240, 100, screen, width, height)
         ]
-    # myGoblin = Characters(goblin_image, 240, 100, screen, width, height)
     myMonster = Characters(monster_image, 240, 100, screen, width, height)
-    # game loop
     stop_game = False
+    #if round is won, increase level by 1
     def won_game(won, level):
         if won == True:
             level+=1
             return level
     loopSong = pygame.mixer.Sound('sounds/music.wav')
+    # game loop
     while not stop_game:
+        #start game song
         loopSong.play()
+        #detect if hero collides with monster
         catch = math.sqrt((myHero.hero_x - myMonster.monster_x)**2 + (myHero.hero_y - myMonster.monster_y)**2)
         # look through user events fired
         if catch >= 25:
@@ -166,31 +177,31 @@ def main():
         #######################################
         # PUT LOGIC TO UPDATE GAME STATE HERE #
         #######################################
-
+        ################################
+        # PUT CUSTOM DISPLAY CODE HERE #
+        ################################
         if catch >= 25:
-
             screen.blit(background_image, (0, 0))
             lvl = pygame.font.SysFont("Comic Sans MS", 30)
             textsurfaceLevel = lvl.render("Level: %d" % (level), True, (255, 255, 255))
             screen.blit(textsurfaceLevel,(0,0))
             change_dir_countdown, direction = myMonster.updateMonster(change_dir_countdown, direction)
             myHero.updateHero()
+            #update goblins from list myGoblins
             for n in range(len(myGoblins)):
                 myGoblins[n].updateGoblin(change_dir_countdown)
 
-
-        ################################
-        # PUT CUSTOM DISPLAY CODE HERE #
-        ################################
-
         for i in range(len(myGoblins)):
+            #detect if hero collides with goblin and set lost to True
             lose = math.sqrt((myHero.hero_x - myGoblins[i].goblin_x)**2 + (myHero.hero_y - myGoblins[i].goblin_y)**2)
             if lose < 25:
                 lost = True
         if lost == True:
+            #stop game music
             loopSong.stop()
             sound = pygame.mixer.Sound('sounds/lose.wav')
             screen.blit(background_image, (0, 0))
+            #show goblins but hide hero and monster
             for i in range(len(myGoblins)):
                 myGoblins[i].screen.blit(myGoblins[i].charImage, (myGoblins[i].goblin_x, myGoblins[i].goblin_y))
             lossText = pygame.font.SysFont("Comic Sans MS", 30)
@@ -201,61 +212,84 @@ def main():
 
             textsurface2 = mySecond.render('Press Enter for yes, or Q to exit the game.', False, (97, 159, 182))
             screen.blit(textsurface2,(60,260))
-
+            #display level
+            lvl = pygame.font.SysFont("Comic Sans MS", 30)
+            textsurface = lvl.render("Level: %d" % (level), False, (255, 255, 255))
+            screen.blit(textsurface,(0,0))
+            #play losing mosing
             sound.play()
+            #listen for if enter is selected to play again
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
+                        sound.stop()
                         screen.blit(background_image, (0, 0))
                         myHero.hero_x = 255
                         myHero.hero_y = 240
-                        myMonster.monster_x = 240
-                        myMonster.monster_y = 100
+                        myMonster.monster_x = randint(40, 440)
+                        myMonster.monster_y = randint(40, 440)
+                        #make sure monster doesn't spawn on hero
+                        myMonster.buffMonst(myMonster.monster_x, myMonster.monster_y)
                         myMonster.updateMonster(change_dir_countdown, direction)
+                        #reset myGoblins to one item
                         myGoblins = [
                             Characters(goblin_image, 240, 100, screen, width, height)
                             ]
+                        level = 1
+                        #update goblin and make sure it doesn't spawn on hero
                         for goblin in myGoblins:
                             goblin.updateGoblin(change_dir_countdown)
-                            goblin.buffCheck(goblin.goblin_x, goblin.goblin_y)
+                            goblin.buffGob(goblin.goblin_x, goblin.goblin_y)
                         myHero.updateHero()
                         lost = False
                     elif event.key == pygame.K_q:
                         stop_game = True
+        #if hero catches monster before goblin catches him
         if catch < 25 and lost != True:
+            #stop game music
             loopSong.stop()
             won = True
             catch = 25
             sound = pygame.mixer.Sound('sounds/win.wav')
             screen.blit(background_image, (0, 0))
             myFirst = pygame.font.SysFont("Comic Sans MS", 30)
-            textsurface = myFirst.render('You win! Would you like to play again?', False, (97, 159, 182))
+            textsurface = myFirst.render('You win! Would you like move on?', False, (97, 159, 182))
             screen.blit(textsurface,(60,240))
 
             mySecond = pygame.font.SysFont("Comic Sans MS", 30)
             textsurface2 = mySecond.render('Press Enter for yes, or Q to exit the game.', False, (97, 159, 182))
             screen.blit(textsurface2,(60,260))
+            #display hero but not goblins or monster
             myHero.screen.blit(myHero.charImage, (myHero.hero_x,
             myHero.hero_y))
             lvl = pygame.font.SysFont("Comic Sans MS", 30)
             textsurface = lvl.render("Level: %d" % (level), False, (255, 255, 255))
             screen.blit(textsurface,(0,0))
+            #play victory music
             sound.play()
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    #listen if enter is selected to keep playing
                     if event.key == pygame.K_RETURN:
+                        #add goblin to list of goblins
                         myGoblins.append(Characters(goblin_image, 240, 100, screen, width, height))
                         screen.blit(background_image, (0, 0))
+                        #increase level
                         level = won_game(won, level)
+                        #set won to false
                         won = False
                         myHero.hero_x = 255
                         myHero.hero_y = 240
+                        #set monster at random point
                         myMonster.monster_x = randint(40, 440)
                         myMonster.monster_y = randint(40, 440)
+                        #make sure monster doesn't spawn on hero
+                        myMonster.buffMonst(myMonster.monster_x, myMonster.monster_y)
                         myMonster.updateMonster(change_dir_countdown, direction)
+                        #update goblin position and make sure they dont spawn on hero
                         for goblin in myGoblins:
                             goblin.updateGoblin(change_dir_countdown)
-                            goblin.buffCheck(goblin.goblin_x, goblin.goblin_y)
+                            goblin.buffGob(goblin.goblin_x, goblin.goblin_y)
                         buffMonst = math.sqrt((myHero.hero_x - myMonster.monster_x)**2 + (myHero.hero_y - myMonster.monster_y)**2)
                         while buffMonst < 70:
                             myMonster.monster_x = randint(40, 440)
